c2580513a7de4cf5afb5fa522a5e61f50f2f9447
Hristo Dzhambov <dzhambov@hotmail.com>
2020-02-11 21:57:40 -0800

    add updated files.

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,36 +1,21 @@
+import { WeatherService } from './../src/weather-service.js';
+
 $(document).ready(function() {
+
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
-    $('#location').val(""); 
+    $('#location').val("");
 
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    (async () => {
+      let weatherService = new WeatherService();
+      const response = await weatherService.getWeatherByCity(city);
+      getElements(response);
+    })();
 
-    request.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      getElement(response);
-     }
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
     }
 
-    request.open("GET", url, true);
-    request.send();
-
-    const getElement = function(response) {
-      $('.showHumidity').text('The humidity in ${city} is ${response.main.humidity}%');
-      $('.showTEmp').text('The temperature in Kelvins is ${response.main.temp} degrees.');
-    }
   });
 });
-
-
-// fetch('api string w/ API_KEY').then((responce) => {responce.json()}).then((obj) => {
-//   let questions = obj.results[0].question;
-// });
-
-// async function bears() {
-//   let apiCall = await fetch('api string w/ API_KEY');
-//   let obj = await apiCall.json();
-
-//   return obj;
-// }
